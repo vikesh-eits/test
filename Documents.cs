d57de1caf77f4283cea4358eba415afbedaad53a
vikesh-eits <vikesh@essorsolutions.com>
2016-10-07 11:47:34 +0530

    Update Documents.cs

--- Documents.cs
+++ Documents.cs
@@ -3,7 +3,7 @@ namespace Utilities.Validations
 {
     public class DocumentExtension
     {
-        /// <summary>
+        /// <summary>test
         /// used to check document extension
         /// </summary>
         /// <param name="imageName">Can be document name, document path</param>
